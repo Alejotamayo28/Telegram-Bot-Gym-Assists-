1571875cfd09e8e3480d672a295f7945e906a53e
Alejadro Vergara <alejandropucca0@gmail.com>
2024-05-22 14:47:03 -0500

    no se que paso

--- src/controller/client/clientController.ts
+++ src/controller/client/clientController.ts
@@ -1,63 +1,63 @@
-import { QueryResult } from "pg";
-import { Request, Response } from 'express';
+import { QueryResult } from "pg";controllerlclient
+import { Request, Response } from "express";
 import { pool } from "../../database/database";
 import { GENERAL_ERROR_HANDLER } from "../../errors";
 import { ResponseHandler } from "../../model/classes/responseManager";
 import { ClientManager } from "../../model/classes/clientManager";
 
-export const insertClientData = async ({ body }: Request, res: Response): Promise<void> => {
-    let client;
-    try {
-        client = await pool.connect();
-        await new ClientManager(client, res).insertClient(body);
-        ResponseHandler.sendSuccessMessage(res, body);
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res);
-        console.error(e);
-    } finally {
-        client && client.release();
-    }
+export const insertClientData = async ({ body }: Request, res: Response,): Promise<void> => {
+  let client;
+  try {
+    client = await pool.connect();
+    await new ClientManager(client, res).insertClient(body);
+    ResponseHandler.sendSuccessMessage(res, body);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);
+  } finally {
+    client && client.release();
+  }
 };
-export const clientData = async ({ params }: Request, res: Response): Promise<void> => {
-    let client;
-    try {
-        const { id } = params
-        client = await pool.connect();
-        const { rowCount, rows }: QueryResult = await new ClientManager(client, res).clientData(id);
-        if (!rowCount) ResponseHandler.sendIdNotFound(res);
-        if (rowCount) ResponseHandler.sendIdFound(res, rows);
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res);
-        console.log(e);
-    } finally {
-        client && client.release();
-    }
+export const clientData = async ({ params }: Request, res: Response,): Promise<void> => {
+  let client;
+  try {
+    const { id } = params;
+    client = await pool.connect();
+    const { rowCount, rows }: QueryResult = await new ClientManager(client, res,).clientData(id);
+    if (!rowCount) ResponseHandler.sendIdNotFound(res);
+    if (rowCount) ResponseHandler.sendIdFound(res, rows);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.log(e);
+  } finally {
+    client && client.release();
+  }
 };
 export const clientDataUpdate = async ({ body, params }: Request, res: Response): Promise<void> => {
-    let client;
-    try {
-        const { id } = params;
-        client = await pool.connect();
-        await new ClientManager(client, res).clientUpdate(id, body);
-        ResponseHandler.sendSuccessMessage(res, body);
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res);
-        console.log(e);
-    } finally {
-        client && client.release();
-    }
+  let client;
+  try {
+    const { id } = params;
+    client = await pool.connect();
+    await new ClientManager(client, res).clientUpdate(id, body);
+    ResponseHandler.sendSuccessMessage(res, body);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.log(e);
+  } finally {
+    client && client.release();
+  }
 };
 export const clientDeleteData = async ({ params }: Request, res: Response) => {
-    let client
-    try {
-        client = await pool.connect()
-        const { id } = params
-        await new ClientManager(client, res).deleteClient(id)
-        ResponseHandler.sendSuccessMessage(res, id)
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res)
-        console.error(e)
-    }
-    client && client.release()
-}
+  let client;
+  try {
+    client = await pool.connect();
+    const { id } = params;
+    await new ClientManager(client, res).deleteClient(id);
+    ResponseHandler.sendSuccessMessage(res, id);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);ByteLengthQueuingStrategy
+  }
+  client && client.release();
+};
 
--- src/controller/workout/workoutController.ts
+++ src/controller/workout/workoutController.ts
@@ -4,67 +4,69 @@ import { ResponseHandler } from "../../model/classes/responseManager";
 import { WorkoutManager } from "../../model/classes/workoutManager";
 import { Response, Request } from "express";
 
-export const insertWorkout = async ({ params, body }: Request, res: Response): Promise<void> => {
-    let client;
-    try {
-        const { id } = params
-        client = await pool.connect();
-        if (await new WorkoutManager(client, res).verifyWorkout(id, body) !== 0) {
-            ResponseHandler.sendExerciseExists(res);
-        } else {
-            await new WorkoutManager(client, res).insertWorkout(id, body);
-            ResponseHandler.sendSuccessMessage(res, body);
-        }
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res);
-        console.error(e);
-    } finally {
-        client && client.release();
+export const insertWorkout = async ({ params, body }: Request, res: Response,): Promise<void> => {
+  let client;
+  try {
+    const { id } = params;
+    client = await pool.connect();
+    if ((await new WorkoutManager(client, res).verifyWorkout(id, body)) !== 0) {
+      ResponseHandler.sendExerciseExists(res);
+    } else {
+      await new WorkoutManager(client, res).insertWorkout(id, body);
+      ResponseHandler.sendSuccessMessage(res, body);
     }
-}
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);
+  } finally {
+    client && client.release();
+  }
+};
 
 export const workoutData = async ({ params, body }: Request, res: Response) => {
-    let client
-    try {
-        const { id } = params
-        client = await pool.connect()
-        const response = await new WorkoutManager(client, res).workoutData(id, body)
-        ResponseHandler.sendSuccessMessage(res, response?.rows)
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res)
-        console.error(e)
-    } finally {
-        client && client.release()
-    }
-}
+  let client;
+  try {
+    const { id } = params;
+    client = await pool.connect();
+    const response = await new WorkoutManager(client, res).workoutData(
+      id,
+      body,
+    );
+    ResponseHandler.sendSuccessMessage(res, response?.rows);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);
+  } finally {
+    client && client.release();
+  }
+};
 
-export const deleteWorkoutData = async ({ params, body }: Request, res: Response) => {
-    let client
-    try {
-        client = await pool.connect()
-        const { id } = params
-        await new WorkoutManager(client, res).deleteWorkout(id, body)
-        ResponseHandler.sendSuccessMessage(res, body)
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res)
-        console.error(e)
-    } finally {
-        client && client.release()
-    }
-}
+export const deleteWorkoutData = async ({ params, body }: Request, res: Response,) => {
+  let client;
+  try {
+    client = await pool.connect();
+    const { id } = params;
+    await new WorkoutManager(client, res).deleteWorkout(id, body);
+    ResponseHandler.sendSuccessMessage(res, body);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);
+  } finally {
+    client && client.release();
+  }
+};
 
-export const UpdateWorkoutData = async ({ params, body }: Request, res: Response) => {
-    let client
-    try {
-        client = await pool.connect()
-        const { id } = params
-        await new WorkoutManager(client, res).UpdateWorkout(id, body)
-        ResponseHandler.sendSuccessMessage(res, body)
-    } catch (e) {
-        GENERAL_ERROR_HANDLER(e, res)
-        console.error(e)
-    } finally {
-        client && client.release()
-    }
-}
-
+export const UpdateWorkoutData = async ({ params, body }: Request, res: Response,) => {
+  let client;
+  try {
+    client = await pool.connect();
+    const { id } = params;
+    await new WorkoutManager(client, res).UpdateWorkout(id, body);
+    ResponseHandler.sendSuccessMessage(res, body);
+  } catch (e) {
+    GENERAL_ERROR_HANDLER(e, res);
+    console.error(e);
+  } finally {
+    client && client.release();
+  }
+};
