4463eafa51050458f6cec7178612f0f01a16f436
Alejadro Vergara <alejandropucca0@gmail.com>
2024-05-27 12:38:08 -0500

    req: RequestExt added to clientWorkout

--- src/controller/workout/classes/workoutManager.ts
+++ src/controller/workout/classes/workoutManager.ts
@@ -4,14 +4,18 @@ import { ClientWorkout } from "../../../model/interface/workout";
 import { GENERAL_ERROR_HANDLER } from "../../../errors";
 import { insertWorkoutQuery, getWorkoutDataQuery, getSingleWorkoutDataQuery, updateWorkoutData, deleteWorkoutDataQuery } from "../../../queries/workoutQueries";
 import { ResponseWorkout } from "./responseManager";
+import { RequestExt } from "../../../middlewares/jsonWebToken/enCryptHelper";
+import { ResponseClient } from "../../client/classes/responseManager";
 
 export class WorkoutManager {
-  constructor(private client: PoolClient, private res: Response) { }
-  public async insertWorkout(id: string, clientWorkout: ClientWorkout): Promise<void> {
+  constructor(private client: PoolClient, private req: RequestExt, private res: Response) { }
+  public async insertWorkout(clientWorkout: ClientWorkout): Promise<void> {
+    const user = this.req.user
     try {
-      const response = await getSingleWorkoutDataQuery(this.client, id, clientWorkout)
+      if (!user) return ResponseClient.clientNotFound(this.res)
+      const response = await getSingleWorkoutDataQuery(this.client, user.id, clientWorkout)
       if (!response.rowCount) {
-        await insertWorkoutQuery(this.client, id, clientWorkout)
+        await insertWorkoutQuery(this.client, user.id, clientWorkout)
         return ResponseWorkout.workoutInsert(this.res)
       } else return ResponseWorkout.workoutAlreadyExists(this.res)
     } catch (e) {
@@ -19,9 +23,11 @@ export class WorkoutManager {
       console.error(`Error inserting workout: `, e)
     }
   }
-  public async workoutData(id: string, clientWorkout: ClientWorkout) {
+  public async workoutData(clientWorkout: ClientWorkout) {
+    const user = this.req.user
     try {
-      const response: QueryResult = await getWorkoutDataQuery(this.client, id, clientWorkout)
+      if (!user) return ResponseClient.clientNotFound(this.res)
+      const response: QueryResult = await getWorkoutDataQuery(this.client, user.id, clientWorkout)
       if (!response.rowCount) return ResponseWorkout.workoutDataNotFound(this.res)
       return ResponseWorkout.workoutData(this.res, response.rows)
     } catch (e) {
@@ -29,26 +35,29 @@ export class WorkoutManager {
       console.error(`Error getting workout Data: `, e)
     }
   }
-  public async UpdateWorkout(id: any, clientWorkout: ClientWorkout) {
+  public async UpdateWorkout(clientWorkout: ClientWorkout) {
+    const user = this.req.user
     try {
+      if (!user) return ResponseClient.clientNotFound(this.res)
       const { series, reps, kg } = clientWorkout
-      const response: QueryResult = await getSingleWorkoutDataQuery(this.client, id, clientWorkout)
-      type OmitTwo<T, K1 extends keyof T, K2 extends keyof T> = Omit<T, K1 | K2>
-      const data: OmitTwo<ClientWorkout, 'day', 'name'> = {
+      const response: QueryResult = await getSingleWorkoutDataQuery(this.client, user.id, clientWorkout)
+      const data: Partial<ClientWorkout> = {
         series: series ?? response.rows[0].series,
         reps: reps ?? response.rows[0].reps,
         kg: kg ?? response.rows[0].kg
       }
-      await updateWorkoutData(this.client, id, data)
+      await updateWorkoutData(this.client, user.id, data)
       return ResponseWorkout.workoutUpdate(this.res)
     } catch (e) {
       GENERAL_ERROR_HANDLER(e, this.res)
       console.error(`Error updating workout data: `, e)
     }
   }
-  public async deleteWorkout(id: any, clientWorkout: ClientWorkout) {
+  public async deleteWorkout(clientWorkout: ClientWorkout) {
+    const user = this.req.user
     try {
-      await deleteWorkoutDataQuery(this.client, id, clientWorkout)
+      if(!user) return ResponseClient.clientNotFound(this.res)
+      await deleteWorkoutDataQuery(this.client, user.id, clientWorkout)
       return ResponseWorkout.workoutDelete(this.res)
     } catch (e) {
       GENERAL_ERROR_HANDLER(e, this.res)
--- src/controller/workout/workoutController.ts
+++ src/controller/workout/workoutController.ts
@@ -2,13 +2,13 @@ import { pool } from "../../database/database";
 import { GENERAL_ERROR_HANDLER } from "../../errors";
 import { Response, Request } from "express";
 import { WorkoutManager } from "./classes/workoutManager";
+import { RequestExt } from "../../middlewares/jsonWebToken/enCryptHelper";
 
-export const insertWorkout = async ({ params, body }: Request, res: Response,): Promise<void> => {
+export const insertWorkout = async (req: RequestExt, res: Response,): Promise<void> => {
   let client;
   try {
-    const { id } = params;
     client = await pool.connect();
-    await new WorkoutManager(client, res).insertWorkout(id, body);
+    await new WorkoutManager(client, req, res).insertWorkout(req.body);
   } catch (e) {
     GENERAL_ERROR_HANDLER(e, res);
     console.error(`Error controller "insertWorkout": `, e);
@@ -17,12 +17,11 @@ export const insertWorkout = async ({ params, body }: Request, res: Response,): 
   }
 };
 
-export const workoutData = async ({ params, body }: Request, res: Response) => {
+export const workoutData = async (req: RequestExt, res: Response) => {
   let client;
   try {
-    const { id } = params;
     client = await pool.connect();
-    await new WorkoutManager(client, res).workoutData(id, body);
+    await new WorkoutManager(client, req, res).workoutData(req.body);
   } catch (e) {
     GENERAL_ERROR_HANDLER(e, res);
     console.error(`Error controller "workoutData": `, e);
@@ -31,26 +30,24 @@ export const workoutData = async ({ params, body }: Request, res: Response) => {
   }
 };
 
-export const deleteWorkoutData = async ({ params, body }: Request, res: Response,) => {
+export const deleteWorkoutData = async (req: RequestExt, res: Response,) => {
   let client;
   try {
     client = await pool.connect();
-    const { id } = params;
-    await new WorkoutManager(client, res).deleteWorkout(id, body);
+    await new WorkoutManager(client, req, res).deleteWorkout(req.body);
   } catch (e) {
     GENERAL_ERROR_HANDLER(e, res);
-    console.error(`Error "deleteWorkoutData": `, e);
+    console.error(`Error controller "deleteWorkoutData": `, e);
   } finally {
     client && client.release();
   }
 };
 
-export const UpdateWorkoutData = async ({ params, body }: Request, res: Response,) => {
+export const UpdateWorkoutData = async (req: RequestExt, res: Response,) => {
   let client;
   try {
     client = await pool.connect();
-    const { id } = params;
-    await new WorkoutManager(client, res).UpdateWorkout(id, body);
+    await new WorkoutManager(client, req, res).UpdateWorkout(req.body);
   } catch (e) {
     GENERAL_ERROR_HANDLER(e, res);
     console.error(`Erorr controller "UpdateWorkoutData": `, e);
